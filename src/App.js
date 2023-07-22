dec347ed338eae90ee4b9ff8d04b5174c4916fcb
kiendt <dotrungkien6987@gmail.com>
2023-07-22 23:10:51 +0700

    first commit basic app matterial UI

--- src/App.js
+++ src/App.js
@@ -1,24 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { Routes, Route, useLocation } from "react-router-dom";
+import Layout from "./pages/Layout";
+import Home from "./pages/Home";
+import JobDetailModal from "./components/JobDetailModal";
+import { useContext } from "react";
+
 
 function App() {
+  const location = useLocation();
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Routes
+        location={
+          location.state?.backgroundLocation
+            ? location.state.backgroundLocation
+            : location
+        }
+      >
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+         
+        </Route>
+        <Route
+          path="*"
+          element={
+            <main>
+              <p>There's nothing here!</p>
+            </main>
+          }
+        />
+      </Routes>
+         </>
   );
 }
 
